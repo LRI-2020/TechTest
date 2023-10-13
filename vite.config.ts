89343a7dc633976b807970dcd229a8c61997fd64
LaylaR <layla.richard.2111@gmail.com>
2023-10-13 12:57:35 +0200

    Add bootstrap

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+﻿// vite.config.js
+import path from "path";
+
+// @ts-ignore
+console.log(path.resolve(__dirname, 'src'));
+export default {
+
+    base: "/TechTest",
+
+    root: path.resolve(__dirname, ''),
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    }
+       
+    // config options
+}
